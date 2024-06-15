7881fea89d16b2d4b3789584ce798ceb76c2383f
Passthem <Passthem@163.com>
2024-06-15 20:25:18 +0800

    删除不再使用的项

--- src/config.py
+++ src/config.py
@@ -14,9 +14,6 @@ class Config(BaseModel):
     # 在发送「小镜！！！」的时候，遇到特殊的 QQ 号，回复特殊的内容
     custom_replies: dict[str, str] = {}
 
-    # 对于「小镜！！！」后面的正则表达式匹配规则，这里不应该设置太宽泛，以免出现意料之外的消息
-    re_match_rule: str = "^[!！?？。.,， 1;；：:'‘’\"“”]+$"
-
     # 是否预先画好小哥的图片
     predraw_images: bool = False
 
