ea3c4e5daf4a545c83f88de3432e4d3f96443161
Passthem <Passthem@163.com>
2024-06-16 03:09:11 +0800

    让 SQLite 支持外键 CASCADE

--- src/common/db.py
+++ src/common/db.py
@@ -1,13 +1,22 @@
+import sqlalchemy
+import sqlalchemy.event
+from sqlalchemy import PoolProxiedConnection
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine, async_sessionmaker
-
 from src.config import config
 
 
 _engine = create_async_engine(config.sqlalchemy_database_url)
 
 _async_session_factory = async_sessionmaker(
-    _engine, class_=AsyncSession, expire_on_commit=True
+    _engine, class_=AsyncSession, expire_on_commit=False, autoflush=False
 )
+
+
+@sqlalchemy.event.listens_for(_engine.sync_engine, "connect")
+def set_sqlite_pragma(dbapi_connection: PoolProxiedConnection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=ON")
+    cursor.close()
 
 
 def get_session():
