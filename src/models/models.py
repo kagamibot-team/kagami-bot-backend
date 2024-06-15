b993c618f567b05b4b4d64c69d14befacd9c0609
Passthem <Passthem@163.com>
2024-06-15 20:50:20 +0800

    删掉所有不用了的 __init__

--- src/models/models.py
+++ src/models/models.py
@@ -1,4 +1,3 @@
-import functools
 import os
 from nonebot_plugin_orm import Model
 from sqlalchemy import Column, ForeignKey, Index, Integer
@@ -17,9 +16,6 @@ class Global(Model, BaseMixin):
 
     __tablename__ = "catch_global"
 
-    # def __init__(self, catch_interval: float = 3600):
-    #     super().__init__(catch_interval=catch_interval)
-
     catch_interval: Mapped[float] = mapped_column(default=3600)
 
 
@@ -29,20 +25,6 @@ class Level(Model, BaseMixin):
     """
 
     __tablename__ = "catch_level"
-
-    # def __init__(
-    #     self,
-    #     name: str = "未命名等级",
-    #     weight: float = 0,
-    #     color_code: str = "#9e9d95",
-    #     price: float = 0,
-    # ):
-    #     super().__init__(
-    #         name=name,
-    #         weight=weight,
-    #         color_code=color_code,
-    #         price=price,
-    #     )
 
     name: Mapped[str] = mapped_column(default="未命名等级", unique=True)
     sorting_priority: Mapped[int] = mapped_column(default=0)
@@ -67,12 +49,6 @@ class LevelAltName(Model, BaseMixin, AltNameMixin):
 
     __tablename__ = "catch_level_alt_name"
 
-    # def __init__(self, level: Level, name: str = ""):
-    #     super().__init__(
-    #         level=level,
-    #         name=name,
-    #     )
-
     level_id = Column(Integer, ForeignKey("catch_level.data_id"))
     level: Mapped[Level] = relationship(back_populates="alt_names", lazy="subquery")
 
@@ -83,12 +59,6 @@ class Tag(Model, BaseMixin):
     """
 
     __tablename__ = "catch_tag"
-
-    # def __init__(self, tag_name: str = "", tag_args: str = ""):
-    #     super().__init__(
-    #         tag_name=tag_name,
-    #         tag_args=tag_args,
-    #     )
 
     tag_name: Mapped[str] = mapped_column(default="")
     tag_args: Mapped[str] = mapped_column(default="")
@@ -111,12 +81,6 @@ class LevelTagRelation(Model, BaseMixin):
 
     __tablename__ = "catch_level_tag_relation"
 
-    # def __init__(self, level: Level, tag: Tag):
-    #     super().__init__(
-    #         level=level,
-    #         tag=tag,
-    #     )
-
     level_id = Column(Integer, ForeignKey("catch_level.data_id"))
     level: Mapped[Level] = relationship(back_populates="tags", lazy="subquery")
 
@@ -126,20 +90,6 @@ class LevelTagRelation(Model, BaseMixin):
 
 class Award(Model, BaseMixin):
     __tablename__ = "catch_award"
-
-    # def __init__(
-    #     self,
-    #     level: Level,
-    #     img_path: str = DEFAULT_IMG,
-    #     name: str = "",
-    #     description: str = "",
-    # ):
-    #     super().__init__(
-    #         level=level,
-    #         img_path=img_path,
-    #         name=name,
-    #         description=description,
-    #     )
 
     img_path: Mapped[str] = mapped_column(default=DEFAULT_IMG)
     name: Mapped[str] = mapped_column(default="", unique=True)
@@ -171,12 +121,6 @@ class AwardAltName(Model, BaseMixin, AltNameMixin):
 
     __tablename__ = "catch_award_alt_name"
 
-    # def __init__(self, award: Award, name: str = ""):
-    #     super().__init__(
-    #         award=award,
-    #         name=name,
-    #     )
-
     award_id = Column(Integer, ForeignKey("catch_award.data_id"))
     award: Mapped[Award] = relationship(back_populates="alt_names", lazy="subquery")
 
@@ -187,12 +131,6 @@ class AwardTagRelation(Model, BaseMixin):
     """
 
     __tablename__ = "catch_award_tag_relation"
-
-    # def __init__(self, award: Award, tag: Tag):
-    #     super().__init__(
-    #         award=award,
-    #         tag=tag,
-    #     )
 
     award_id = Column(Integer, ForeignKey("catch_award.data_id"))
     award: Mapped[Award] = relationship(back_populates="tags", lazy="subquery")
@@ -208,13 +146,6 @@ class StorageStats(Model, BaseMixin):
         Index("storage_stat_index", "target_user_id", "target_award_id", unique=True),
     )
 
-    # def __init__(self, user: "User", award: Award, count: int = 0):
-    #     super().__init__(
-    #         user=user,
-    #         award=award,
-    #         count=count,
-    #     )
-
     target_user_id = Column(Integer, ForeignKey("catch_user_data.data_id"))
     target_award_id = Column(Integer, ForeignKey("catch_award.data_id"))
     count: Mapped[int] = mapped_column(default=0)
@@ -226,13 +157,6 @@ class StorageStats(Model, BaseMixin):
 class UsedStats(Model, BaseMixin):
     __tablename__ = "catch_award_stats"
 
-    # def __init__(self, user: "User", award: Award, count: int = 0):
-    #     super().__init__(
-    #         user=user,
-    #         award=award,
-    #         count=count,
-    #     )
-
     target_user_id = Column(Integer, ForeignKey("catch_user_data.data_id"))
     target_award_id = Column(Integer, ForeignKey("catch_award.data_id"))
     count: Mapped[int] = mapped_column(default=0)
@@ -243,24 +167,6 @@ class UsedStats(Model, BaseMixin):
 
 class User(Model, BaseMixin):
     __tablename__ = "catch_user_data"
-
-    # def __init__(
-    #     self,
-    #     qq_id: int,
-    #     money: float = 0.0,
-    #     pick_count_remain: int = 0,
-    #     pick_count_last_calculated: float = 0,
-    #     pick_time_delta: float = 7200,
-    #     pick_max_cache: int = 1,
-    # ):
-    #     super().__init__(
-    #         qq_id=qq_id,
-    #         money=money,
-    #         pick_count_remain=pick_count_remain,
-    #         pick_count_last_calculated=pick_count_last_calculated,
-    #         pick_time_delta=pick_time_delta,
-    #         pick_max_cache=pick_max_cache,
-    #     )
 
     qq_id: Mapped[str] = mapped_column(unique=True, index=True)
 
@@ -288,16 +194,6 @@ class User(Model, BaseMixin):
 class UsedSkin(Model, BaseMixin):
     __tablename__ = "catch_skin_record"
 
-    # def __init__(
-    #     self,
-    #     user: User,
-    #     skin: "Skin",
-    # ):
-    #     super().__init__(
-    #         user=user,
-    #         skin=skin,
-    #     )
-
     user_id = Column(Integer, ForeignKey("catch_user_data.data_id"))
     skin_id = Column(Integer, ForeignKey("catch_skin.data_id"))
 
@@ -308,16 +204,6 @@ class UsedSkin(Model, BaseMixin):
 class OwnedSkin(Model, BaseMixin):
     __tablename__ = "catch_skin_own_record"
 
-    # def __init__(
-    #     self,
-    #     user: User,
-    #     skin: "Skin",
-    # ):
-    #     super().__init__(
-    #         user=user,
-    #         skin=skin,
-    #     )
-
     user_id = Column(Integer, ForeignKey("catch_user_data.data_id"))
     skin_id = Column(Integer, ForeignKey("catch_skin.data_id"))
 
@@ -327,22 +213,6 @@ class OwnedSkin(Model, BaseMixin):
 
 class Skin(Model, BaseMixin):
     __tablename__ = "catch_skin"
-
-    # def __init__(
-    #     self,
-    #     name: str,
-    #     award: "Award",
-    #     extra_description: str = "",
-    #     image: str = DEFAULT_IMG,
-    #     price: float = -1.0,
-    # ):
-    #     super().__init__(
-    #         name=name,
-    #         award=award,
-    #         extra_description=extra_description,
-    #         image=image,
-    #         price=price,
-    #     )
 
     name: Mapped[str] = mapped_column()
     extra_description: Mapped[str] = mapped_column(default="")
@@ -370,16 +240,6 @@ class Skin(Model, BaseMixin):
 class SkinTagRelation(Model, BaseMixin):
     __tablename__ = "catch_skin_tag_relation"
 
-    # def __init__(
-    #     self,
-    #     skin: "Skin",
-    #     tag: "Tag",
-    # ):
-    #     super().__init__(
-    #         skin=skin,
-    #         tag=tag,
-    #     )
-
     skin_id = Column(Integer, ForeignKey("catch_skin.data_id"))
     tag_id = Column(Integer, ForeignKey("catch_tag.data_id"))
     skin: Mapped[Skin] = relationship(back_populates="tags", lazy="subquery")
@@ -392,12 +252,6 @@ class SkinAltName(Model, BaseMixin, AltNameMixin):
     """
 
     __tablename__ = "catch_skin_alt_name"
-
-    # def __init__(self, skin: Skin, name: str = ""):
-    #     super().__init__(
-    #         skin=skin,
-    #         name=name,
-    #     )
 
     skin_id = Column(Integer, ForeignKey("catch_skin.data_id"))
     skin: Mapped[Skin] = relationship(back_populates="alt_names", lazy="subquery")
