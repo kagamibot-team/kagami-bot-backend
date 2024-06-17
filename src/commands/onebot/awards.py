1b150c8658d1a23a713363f8258451bbd53aa9d4
Passthem <Passthem@163.com>
2024-06-17 18:21:49 +0800

    用已有可复用代码，缩小展示指令长度

--- src/commands/onebot/awards.py
+++ src/commands/onebot/awards.py
@@ -14,80 +14,17 @@ async def _(ctx: OnebotContext, session: AsyncSession, result: Arparma):
     if name is None:
         return
 
-    target = (
-        Award.data_id,
-        Award.img_path,
-        Award.name,
-        Award.description,
-        Level.name,
-        Level.color_code,
-    )
-
-    query1 = (
-        select(*target)
-        .join(Level, Level.data_id == Award.level_id)
-        .filter(Award.name == name)
-    )
-    award = (await session.execute(query1)).one_or_none()
-
-    if award is None:
-        query2 = (
-            select(*target)
-            .join(Level, Level.data_id == Award.level_id)
-            .filter(Award.alt_names.any(AwardAltName.name == name))
-        )
-        award = (await session.execute(query2)).one_or_none()
+    award = await getAidByName(session, name)
 
     if award is None:
         await ctx.reply(UniMessage(f"没有叫 {name} 的小哥"))
         return
 
-    award = award.tuple()
-
-    ac = (
-        await session.execute(
-            select(StorageStats.count)
-            .filter(StorageStats.target_user_id == user)
-            .filter(StorageStats.target_award_id == award[0])
-        )
-    ).scalar_one_or_none() or 0
-    au = (
-        await session.execute(
-            select(UsedStats.count)
-            .filter(UsedStats.target_user_id == user)
-            .filter(UsedStats.target_award_id == award[0])
-        )
-    ).scalar_one_or_none() or 0
-
-    if ac + au <= 0:
+    if await getStatistics(session, user, award) <= 0:
         await ctx.reply(UniMessage(f"你还没有遇到过叫做 {name} 的小哥"))
         return
 
-    skinQuery = (
-        select(Skin.name, Skin.extra_description, Skin.image)
-        .filter(Skin.applied_award_id == award[0])
-        .filter(Skin.used_skins.any(UsedSkin.user_id == user))
-    )
-    skin = (await session.execute(skinQuery)).one_or_none()
-
-    info = AwardInfo(
-        awardId=award[0],
-        awardImg=award[1],
-        awardName=award[2],
-        awardDescription=award[3],
-        levelName=award[4],
-        color=award[5],
-        skinName=None,
-    )
-
-    if skin:
-        skin = skin.tuple()
-        info.skinName = skin[0]
-        info.awardDescription = (
-            skin[1] if len(skin[1].strip()) > 0 else info.awardDescription
-        )
-        info.awardImg = skin[2]
-
+    info = await getAwardInfo(session, user, award)
     nameDisplay = info.awardName
 
     if info.skinName is not None:
