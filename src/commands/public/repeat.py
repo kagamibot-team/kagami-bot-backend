ec2355cbeea116a69bcb4fa6669a256a350ed2f2
Alpha-QX <2046826359@qq.com>
2024-06-21 01:20:07 +0800

    修复了复读镜

--- src/commands/public/repeat.py
+++ src/commands/public/repeat.py
@@ -15,7 +15,6 @@ async def repeat(ctx: PublicContext, match: Match[str]):
     msg0 = message[0]
     n = match_list[0]
     msg0o : str = msg0.text
-    logger.info(msg0o)
     if len(message) == 1:
         while msg0o.startswith(n) or msg0o.startswith(" "):
             if msg0o.startswith(n) and len(msg0o) > len(n):
@@ -36,22 +35,18 @@ async def repeat(ctx: PublicContext, match: Match[str]):
                 msg0o = ""
             else:
                 break
-    logger.info(msg0o)
+    
     _output = UniMessage.text(msg0o)
-
     for msg in message[1:]:
         if isinstance(msg, Text):
             _output += msg.text
-            logger.info(_output)
         elif isinstance(msg,Emoji):
             _output += msg
-            logger.info(_output)
         elif isinstance(msg,At):
             _output += msg
-            logger.info(_output)
         else:
             break
-    if "回收大于0" or "回收小于" in _output:
+    if re.match("^回收大于0(.)*",UniMessage.extract_plain_text(_output)) or re.match("^回收小于(.)*",UniMessage.extract_plain_text(_output)):
         await ctx.reply(UniMessage("这样做小镜会伤心的！"))
         return
     
