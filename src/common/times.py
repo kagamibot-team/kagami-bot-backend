84ba50d0a97a7b181bbd08e49e7eabc9b77a3c8c
Passthem <Passthem@163.com>
2024-06-20 17:34:38 +0800

    :penguin:添加 now_datetime() 方法以获取当前时间

--- src/common/times.py
+++ src/common/times.py
@@ -2,6 +2,10 @@ import datetime
 import pytz
 
 
+def now_datetime():
+    return to_utc8(datetime.datetime.now())
+
+
 def to_utc8(dt: datetime.datetime):
     return dt.astimezone(pytz.timezone("Asia/Shanghai"))
 
