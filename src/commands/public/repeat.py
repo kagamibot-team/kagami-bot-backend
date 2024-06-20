482651c4c9ae3c8b8ba3ebc273b36290a3e90072
Passthem <Passthem@163.com>
2024-06-21 02:22:16 +0800

    :art:调整复读镜的一些代码

--- src/commands/public/repeat.py
+++ src/commands/public/repeat.py
@@ -3,18 +3,15 @@ from nonebot_plugin_alconna import Image, Text, At, Emoji
 import re
 
 
-
-
 @listenPublic()
 @matchRegex("^(复读镜)+ ?(.+|\\s+)+")
-
-
 async def repeat(ctx: PublicContext, match: Match[str]):
     message = await ctx.getMessage()
-    msg0 : Text
     msg0 = message[0]
+    if not isinstance(msg0, Text):
+        return
     n = match_list[0]
-    msg0o : str = msg0.text
+    msg0o: str = msg0.text
     if len(message) == 1:
         while msg0o.startswith(n) or msg0o.startswith(" "):
             if msg0o.startswith(n) and len(msg0o) > len(n):
@@ -35,21 +32,24 @@ async def repeat(ctx: PublicContext, match: Match[str]):
                 msg0o = ""
             else:
                 break
-    
+
     _output = UniMessage.text(msg0o)
     for msg in message[1:]:
         if isinstance(msg, Text):
             _output += msg.text
-        elif isinstance(msg,Emoji):
+        elif isinstance(msg, Emoji):
             _output += msg
-        elif isinstance(msg,At):
+        elif isinstance(msg, At):
             _output += msg
         else:
             break
-    if re.match("^回收大于0(.)*",UniMessage.extract_plain_text(_output)) or re.match("^回收小于(.)*",UniMessage.extract_plain_text(_output)):
+    if re.match("^回收大于0(.)*", _output.extract_plain_text()) or re.match(
+        "^回收小于(.)*", _output.extract_plain_text()
+    ):
         await ctx.reply(UniMessage("这样做小镜会伤心的！"))
         return
-    
+
     await ctx.send(_output)
+
 
 match_list = ["复读镜"]
