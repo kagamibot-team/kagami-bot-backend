29a5edb81ababdebaa3a399c956551d2fcb53d12
Passthem <Passthem@163.com>
2024-06-21 23:16:46 +0800

    :white_check_mark:调整小镜！！！的testcase

--- tests/test_events.py
+++ tests/test_events.py
@@ -6,7 +6,15 @@ from src.imports import *
 
 class TestEvents(SQLTestCase):
     async def test_kagami(self):
-        ctx = MockUniMessageContext(UniMessage().text("小镜！！！"))
+        ctx = MockUniMessageContext(UniMessage().text("小镜！！！!!!"))
         self.assertIsInstance(ctx, UniMessageContext)
         await root.emit(ctx)
         self.assertEqual(len(ctx.sent), 1)
+        self.assertEqual(len(ctx.sent[0]), 1)
+        self.assertIsInstance(ctx.sent[0][0], Text)
+        self.assertEqual(str(ctx.sent[0][0]), "在！！！!!!")
+
+        ctx = MockUniMessageContext(UniMessage().text("小镜 我讨厌你"))
+        self.assertIsInstance(ctx, UniMessageContext)
+        await root.emit(ctx)
+        self.assertEqual(len(ctx.sent), 0)
