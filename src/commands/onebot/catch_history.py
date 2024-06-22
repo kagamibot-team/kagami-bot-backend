0b920402ef7413016885abf48ada4877cf23c52b
Mokie <2867161032@qq.com>
2024-06-22 22:44:54 +0800

    喜报格式更改

--- src/commands/onebot/catch_history.py
+++ src/commands/onebot/catch_history.py
@@ -6,13 +6,15 @@ from src.imports import *
 async def _(ctx: GroupContext, _):
     history_list = catch_histroy_list.get_records(ctx.event.group_id)
 
-    messages: list[str] = []
+    message: str = ""
+    records: dict[str, dict[str, list[tuple[str, dict[int, PickDisplay]]]]] = {}
 
     for history in history_list:
         dt = timestamp_to_datetime(history.caught_time)
         dt = to_utc8(dt)
 
-        dt_str = dt.strftime(r"%Y年%m月%d日 %H:%M:%S")
+        dt_str_day = dt.strftime(r"%Y年%m月%d日")
+        dt_str_time = dt.strftime(r"%H:%M:%S")
 
         logger.info(history.uid)
 
@@ -27,18 +29,27 @@ async def _(ctx: GroupContext, _):
         except ActionFailed as e:
             logger.warning(e)
 
-        messages.append(
-            f"{name} 在 {dt_str} 抓到了："
-            + "".join(
-                [
-                    f"\n- {display.name} × {display.pick.delta}"
-                    for _, display in history.displays.items()
-                ]
-            )
-        )
+        if dt_str_day not in records:
+            records[dt_str_day] = {}
+        if name not in records[dt_str_day]:
+            records[dt_str_day][name] = []
+        records[dt_str_day][name].append((dt_str_time, history.displays))
 
-    if len(messages) > 0:
-        await ctx.send(UniMessage().text("===== 喜报 =====\n" + "\n\n".join(messages)))
+    for day, datas in records.items():
+        message += f"~ {day} ~\n"
+
+        for name, get_list in datas.items():
+            message += f"- 玩家 {name}\n"
+            for get_info in get_list:
+                message += f"在 {get_info[0]} 抓到了："
+                message +=  "".join([
+                                        f"{display.name} ×{display.pick.delta} ，"
+                                        for _, display in get_info[1].items()
+                                    ]).rstrip("，") + "；\n"
+            message = message.rstrip("；\n") + "。\n\n"
+
+    if len(message) > 0:
+        await ctx.send(UniMessage().text("===== 喜报 =====\n\n" + message.rstrip("\n")))
     else:
         await ctx.send(
             UniMessage().text(
