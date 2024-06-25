817d8f523d13ee075f0ae4473b6c2335238fa7e0
Passthem <Passthem@163.com>
2024-06-26 01:01:51 +0800

    添加了对常用指令的单元测试

--- tests/test_events.py
+++ tests/test_events.py
@@ -1,5 +1,6 @@
+from src.base.onebot_basic import export_msg
 from .base.basic_test_case import SQLTestCase
-from .base.mock_events import MockUniMessageContext
+from .base.mock_events import MockOnebot, MockOnebotGroupEvent, MockUniMessageContext
 
 from src.imports import *
 
@@ -19,3 +20,74 @@ class TestEvents(SQLTestCase):
         await root.emit(ctx)
         logger.info(ctx.sent)
         self.assertEqual(len(ctx.sent), 0)
+
+    async def test_commands(self):
+        session = get_session()
+        async with session.begin():
+            l = Level(data_id=1, name="1", weight=1)
+            a = Award(level_id=1, name="2")
+
+            session.add(l)
+            session.add(a)
+
+            await session.commit()
+
+        bot = MockOnebot("0")
+        ctx = GroupContext(
+            MockOnebotGroupEvent(
+                user_id=10000,
+                to_me=False,
+                message=export_msg(UniMessage().text("kz")),
+                group_id=20000,
+            ),
+            bot,
+        )
+        self.assertIsInstance(ctx, UniMessageContext)
+        await root.emit(ctx)
+        logger.info([i[0] for i in bot.called_apis])
+        self.assertGreater(len(bot.called_apis), 0)
+
+        bot = MockOnebot("0")
+        ctx = GroupContext(
+            MockOnebotGroupEvent(
+                user_id=10000,
+                to_me=False,
+                message=export_msg(UniMessage().text("zhuajd")),
+                group_id=20000,
+            ),
+            bot,
+        )
+        self.assertIsInstance(ctx, UniMessageContext)
+        await root.emit(ctx)
+        logger.info([i[0] for i in bot.called_apis])
+        self.assertGreater(len(bot.called_apis), 0)
+
+        bot = MockOnebot("0")
+        ctx = GroupContext(
+            MockOnebotGroupEvent(
+                user_id=10000,
+                to_me=False,
+                message=export_msg(UniMessage().text("kc")),
+                group_id=20000,
+            ),
+            bot,
+        )
+        self.assertIsInstance(ctx, UniMessageContext)
+        await root.emit(ctx)
+        logger.info([i[0] for i in bot.called_apis])
+        self.assertGreater(len(bot.called_apis), 0)
+
+        bot = MockOnebot("0")
+        ctx = GroupContext(
+            MockOnebotGroupEvent(
+                user_id=10000,
+                to_me=False,
+                message=export_msg(UniMessage().text("xb")),
+                group_id=20000,
+            ),
+            bot,
+        )
+        self.assertIsInstance(ctx, UniMessageContext)
+        await root.emit(ctx)
+        logger.info([i[0] for i in bot.called_apis])
+        self.assertGreater(len(bot.called_apis), 0)
