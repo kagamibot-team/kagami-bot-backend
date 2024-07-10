fe8830961a33bedf6c05243c67bd0294b4d01f36
Passthem <Passthem@163.com>
2024-07-11 00:35:55 +0800

    调整 SQLite3 的优化范围限制

--- src/base/db.py
+++ src/base/db.py
@@ -1,12 +1,11 @@
-import sqlite3
 import sqlalchemy
 import sqlalchemy.event
 from sqlalchemy import PoolProxiedConnection, text
 from sqlalchemy.ext.asyncio import (
+    AsyncEngine,
     AsyncSession,
     async_sessionmaker,
     create_async_engine,
-    AsyncEngine,
 )
 
 from src.common.config import config
@@ -17,15 +16,19 @@ _async_session_factory = async_sessionmaker(
     sqlEngine, class_=AsyncSession, expire_on_commit=False, autoflush=False
 )
 
+if config.sqlalchemy_database_url.startswith("sqlite+aiosqlite:///"):
+    @sqlalchemy.event.listens_for(sqlEngine.sync_engine, "connect")
+    def set_sqlite_pragma(dbapi_connection: PoolProxiedConnection, _):
+        """
+        为 SQLite3 添加优化
+        """
 
-@sqlalchemy.event.listens_for(sqlEngine.sync_engine, "connect")
-def set_sqlite_pragma(dbapi_connection: PoolProxiedConnection, _):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA foreign_keys=ON;")
-    cursor.execute("PRAGMA threading_mode = MULTITHREAD;")
-    cursor.execute("PRAGMA busy_timeout = 30000;")
-    cursor.execute("PRAGMA journal_mode=WAL;")
-    cursor.close()
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.execute("PRAGMA threading_mode = MULTITHREAD;")
+        cursor.execute("PRAGMA busy_timeout = 30000;")
+        cursor.execute("PRAGMA journal_mode=WAL;")
+        cursor.close()
 
 
 async def manual_checkpoint(engine: AsyncEngine = sqlEngine):
