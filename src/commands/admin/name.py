9486fd1b141debf2f29580d75b1b79ba1124fabd
Passthem <Passthem@163.com>
2024-07-29 13:23:46 +0800

    设置名字

---
+++ src/commands/admin/name.py
@@ -0,0 +1,26 @@
+from typing import Any
+from src.base.command_events import OnebotContext
+from src.common.decorators.command_decorators import (
+    listenOnebot,
+    matchAlconna,
+    requireAdmin,
+)
+from arclet.alconna import Alconna, Arg, ArgFlag, Arparma
+
+from src.core.unit_of_work import get_unit_of_work
+
+
+@listenOnebot()
+@requireAdmin()
+@matchAlconna(
+    Alconna(["::"], "叫", Arg("QQ号", int), Arg("名字", str, flags=[ArgFlag.OPTIONAL]))
+)
+async def _(ctx: OnebotContext, res: Arparma[Any]):
+    qqid = res.query[int]("QQ号", -1)
+    name = res.query[str]("名字")
+
+    async with get_unit_of_work() as uow:
+        uid = await uow.users.get_uid(qqid)
+        await uow.users.set_name(uid, name)
+
+    await ctx.reply("ok")
