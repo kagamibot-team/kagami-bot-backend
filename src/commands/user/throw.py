c2381190fba0cdcee3b87052ef7b87c8a8112790
Passthem <Passthem@163.com>
2024-08-13 22:00:25 +0800

    补充关键词和限频

--- src/commands/user/throw.py
+++ src/commands/user/throw.py
@@ -1,3 +1,4 @@
+import time
 from nonebot_plugin_alconna import At, Emoji, Reply, Text, UniMessage
 from src.base.command_events import OnebotContext
 from src.common.command_decorators import listen_message
@@ -8,8 +9,16 @@ from nonebot.adapters.onebot.v11 import MessageSegment
 from src.core.unit_of_work import get_unit_of_work
 
 
+FREQUENCY_LIMIT: dict[int, float] = {}
+
+
 @listen_message()
 async def _(ctx: OnebotContext):
+    FREQUENCY_LIMIT.setdefault(ctx.sender_id, 0)
+    if time.time() - FREQUENCY_LIMIT[ctx.sender_id] < 10:
+        return
+    FREQUENCY_LIMIT[ctx.sender_id] = time.time()
+
     is_throw = False
     is_poop = False
     target: set[int] = set()
@@ -17,10 +26,23 @@ async def _(ctx: OnebotContext):
     for segment in ctx.message:
         if isinstance(segment, Text):
             text = segment.text
-            for i in ("史", "石", "屎", "粑粑", "💩", "大便", "便便", "大变", "大便"):
+            for i in (
+                "史",
+                "石",
+                "屎",
+                "粑粑",
+                "💩",
+                "大便",
+                "便便",
+                "大变",
+                "大便",
+                "答辩",
+                "矢",
+                "十",
+            ):
                 if i in text:
                     is_poop = True
-            for i in ("丢", "扔", "抛", "吃", "赤"):
+            for i in ("丢", "扔", "抛", "吃", "赤", "吔", "叱", "持"):
                 if i in text:
                     is_throw = True
             for i in ("不", "别", "莫", "讨厌"):
@@ -70,10 +92,65 @@ async def _(ctx: OnebotContext):
         if successed:
             await uow.inventories.give(tuid, poop, 1)
 
-    msg = UniMessage.text("你向 ").at(user_id=str(p)).text(" 扔出去了一个粑粑小哥")
-    if successed:
-        msg.text("，扔中了，粑粑小哥爬到了他的库存里面")
-    else:
-        msg.text("，没扔中，粑粑小哥在地里烂掉了")
+        count = await uow.inventories.get_storage(fuid, poop)
 
-    await ctx.reply(msg)
+    msg = (
+        UniMessage.text(
+            get_random().choice(
+                (
+                    "怎么有股臭味啊？原来 ",
+                    "",
+                    "突然，",
+                    "在一瞬间，",
+                    "不留神，",
+                    "在背地里偷笑了一下的 ",
+                    "脚滑的 ",
+                    "肯定不是故意的 ",
+                    "肯定是不小心的 ",
+                )
+            )
+        )
+        .at(user_id=str(ctx.sender_id))
+        .text(" 向 ")
+        .at(user_id=str(p))
+        .text(" 扔出去了一个粑粑小哥，")
+    )
+    if successed:
+        msg.text(
+            get_random().choice(
+                (
+                    "扔中了，粑粑小哥爬到了他的库存里面",
+                    "完美的命中！",
+                    "粑粑小哥奋力跑出加速度，进了他的库存",
+                    "进球了！！！！",
+                    "完美一击！",
+                    "虽然偏了，但粑粑小哥像回旋镖一样命中了他的背后",
+                    "中",
+                    "三分球！",
+                    "这下不用退钱了",
+                    "实心球技术不错！",
+                    "砸中了！",
+                )
+            )
+        )
+    else:
+        msg.text(
+            get_random().choice(
+                (
+                    "没扔中，粑粑小哥在地里烂掉了",
+                    "偏了一点，下次努力吧！",
+                    "粑粑小哥掉到八目鳗穴里了",
+                    "结果不小心扔错了方向。",
+                    "但在空中消失了",
+                    "结果被龙卷风吸走了",
+                    "却不小心掉到了池塘里面污染水质",
+                    "被空中莫名出现的钢筋挡住了",
+                    "但是却被吸入四维空间碎块了",
+                    "这时候，路边的吹风机突然开始运作，粑粑被弹开了",
+                )
+            )
+        )
+
+    msg.text(f"（库存还有 {count} 个粑粑小哥）")
+
+    await ctx.send(msg)
