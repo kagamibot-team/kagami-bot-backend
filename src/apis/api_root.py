cc14b843317687db382af4c4d9e5aef0f8197bdc
Passthem <Passthem@163.com>
2024-09-13 00:10:41 +0800

    Feature/Ping API

--- src/apis/api_root.py
+++ src/apis/api_root.py
@@ -1,3 +1,29 @@
+from enum import Enum
+from typing import Generic, TypeVar
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 router = APIRouter()
+
+
+T = TypeVar("T")
+
+
+class StatusCode(Enum):
+    ok = 0
+    error = 1
+
+
+class APIWrapper(BaseModel, Generic[T]):
+    code: StatusCode = StatusCode.ok
+    msg: str = ""
+    data: T
+
+
+class Ping(APIWrapper[str], BaseModel):
+    data: str = "pong"
+
+
+@router.get("/ping")
+async def ping():
+    return Ping()
