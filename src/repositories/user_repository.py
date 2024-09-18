0157894506113cbd3a354670e99245a0470f36dd
Mokie <2867161032@qq.com>
2024-09-18 21:14:16 +0800

    删除危险指令

--- src/repositories/user_repository.py
+++ src/repositories/user_repository.py
@@ -106,15 +106,6 @@ class UserRepository(DBRepository):
             .values({User.pick_max_cache: User.pick_max_cache + count})
         )
 
-    async def tmp(self):
-        q = delete(User).where(User.qq_id == 3044882834)
-        await self.session.execute(q)
-        await self.session.execute(
-            update(User)
-            .where(User.qq_id == 3095961868)
-            .values({User.qq_id: 3044882834})
-        )
-
     async def get_sign_in_info(self, uid: int) -> tuple[float, int]:
         """获得用户上次签到时间和签到次数
 
