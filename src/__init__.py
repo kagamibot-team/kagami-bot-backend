e37ebb28b6a11a8707e6730d1a194e293a8d536b
Passthem <Passthem@163.com>
2024-09-28 01:20:19 +0800

    添加访问隐藏代码的钩子

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,21 @@
 小镜 Bot 初始化的模块。当载入这个模块时，会自动开始初始化。
 """
 
+import importlib
+
+from loguru import logger
+
+from src.common.config import get_config
+
+
+def load_secret_code():
+    try:
+        module = importlib.import_module("secret")
+        module.init()
+    except ModuleNotFoundError as e:
+        logger.error("请检查是否下载了 secret 代码包！")
+        raise e from e
+
 
 def init_src():
     from src.base.event.event_root import activate_root, root
@@ -12,3 +27,6 @@ def init_src():
     activate_root(root)
     init()
     init_routers()
+
+    if get_config().load_secret_code:
+        load_secret_code()
