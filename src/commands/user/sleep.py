0c67330dab1728afd635f20837af1f07a08525b7
Passthem-desu <Passthem@163.com>
2025-01-19 23:26:28 +0800

    Fix3

--- src/commands/user/sleep.py
+++ src/commands/user/sleep.py
@@ -115,7 +115,7 @@ async def goodnight(ctx: GroupContext, res: Arparma):
         uid = await uow.users.get_uid(ctx.sender_id)
         last_time, count = await uow.users.get_sleep_early_data(uid)
         last_dt = timestamp_to_datetime(last_time).date()
-        if 21 <= now_time.hour < 23 and ((now_time.date() - last_dt).days <= 1 or get_config().safe_sleep):
+        if (21 <= now_time.hour < 23 or get_config().safe_sleep) and ((now_time.date() - last_dt).days <= 1):
             count += 1
         elif 21 <= now_time.hour < 23:
             count = 1
