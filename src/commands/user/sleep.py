952572e1a08426331dc629ea1de732a348659bf8
Passthem <Passthem@163.com>
2025-01-20 22:51:51 +0800

    优化了晚安的逻辑

--- src/commands/user/sleep.py
+++ src/commands/user/sleep.py
@@ -5,6 +5,7 @@
 import asyncio
 import datetime
 import re
+from typing import Any
 
 from arclet.alconna import Alconna, Arg, Arparma, MultiVar
 from nonebot_plugin_alconna import UniMessage
@@ -15,12 +16,7 @@ from src.base.exceptions import (
     SleepToLateException,
     UnknownArgException,
 )
-from src.common.command_deco import (
-    limited,
-    listen_message,
-    match_alconna,
-    require_awake,
-)
+from src.common.command_deco import limited, listen_message, match_alconna
 from src.common.config import get_config
 from src.common.rd import get_random
 from src.common.times import now_datetime, timestamp_to_datetime
@@ -94,82 +90,128 @@ def parse_getup_time(inp: tuple[str] | None) -> tuple[int, int]:
 )
 async def goodnight(ctx: GroupContext, res: Arparma):
     async with get_unit_of_work(ctx.sender_id) as uow:
-        n = now_datetime().timestamp()
+        now_time = now_datetime()
+        now_timestamp = now_time.timestamp()
         uid = await uow.users.get_uid(ctx.sender_id)
-        if await uow.users.get_getup_time(uid) > n:
+
+        if await uow.users.get_getup_time(uid) > now_timestamp:
+            # 如果起床时间比当前时间晚，说明玩家已经晚安了
+            # 这时候应该忽略这个命令
             return
+
+        # 获取玩家期望的起床时间
         arg = res.query[tuple[str]]("getup_time")
         hour, minute = parse_getup_time(arg)
 
-        # 必须将一些逻辑放在 UOW 里面，虽然会导致一些阻塞问题，
-        # 但是这种问题的耗时是微不足道的，而且可以避免出现恶性的
-        # 刷薯片 Bug。
-        now_time = now_datetime()
+        # 计算玩家期望的起床时间
         target = now_time.replace(hour=hour, minute=minute, second=0)
         if target < now_time:
+            # 大概率这个时间是今天的早上，所以需要加一天
             target += datetime.timedelta(days=1)
 
         if hour <= now_time.hour < 21:
+            # 现在还不是睡觉时间，不能睡觉
             raise NotSleepTimeException()
 
-        uid = await uow.users.get_uid(ctx.sender_id)
+        # 更新玩家的起床时间和连胜计数
         last_time, count = await uow.users.get_sleep_early_data(uid)
-        last_dt = timestamp_to_datetime(last_time).date()
-        if (21 <= now_time.hour < 23 or get_config().safe_sleep) and ((now_time.date() - last_dt).days <= 1):
-            count += 1
-        elif 21 <= now_time.hour < 23 or get_config().safe_sleep:
-            count = 1
-        else:
-            count = 0
-        last_time = now_time.timestamp()
+        success = await is_sleep_early(now_time)
+        last_time, count = await update_sleep_data(now_time, last_time, count, success)
 
+        # 计算奖励
+        awarding = get_random().randint(50, 100) if success else 0
+
+        # 保存数据
         await uow.users.update_sleep_early_data(uid, last_time, count)
-
-        awarding = 0
-        if 21 <= now_time.hour < 23 or get_config().safe_sleep:
-            awarding = get_random().randint(50, 100)
-            await uow.chips.add(uid, awarding)
-
         await uow.users.set_getup_time(uid, target.timestamp())
-        name = await uow.users.name(qqid=ctx.sender_id)
-        await StatService(uow).sleep(uid, 21 <= now_time.hour < 23)
+        await StatService(uow).sleep(uid, success)
+        await uow.chips.add(uid, awarding)
 
-    # 渲染消息部分
+        name = await uow.users.name(qqid=ctx.sender_id)
+
+    await reply_messages(ctx, name, awarding, count)
+
+
+async def update_sleep_data(
+    now_time: datetime.datetime,
+    last_time: float,
+    count: int,
+    success: bool,
+) -> tuple[float, int]:
+    """
+    根据当前时间和上次早睡时间更新早睡计数。
+
+    参数:
+        now_time (datetime.datetime): 当前时间。
+        last_dt (datetime.date): 上次早睡的日期。
+        count (int): 当前的早睡计数。
+        success (bool): 是否早睡成功。
+
+    返回:
+        int: 更新后的早睡计数。
+    """
+    if (
+        success
+        and (now_time.date() - timestamp_to_datetime(last_time).date()).days <= 1
+    ):
+        count += 1
+    elif success:
+        count = 1
+    else:
+        count = 0
+    return now_time.timestamp(), count
+
+
+async def is_sleep_early(now_time: datetime.datetime):
+    """
+    判断当前睡觉时间是否胜利。
+
+    参数:
+        now_time (datetime.datetime): 当前时间。
+    """
+    return 21 <= now_time.hour < 23 or get_config().safe_sleep
+
+
+GOODNIGHT_MESSAGES: list[str | UniMessage[Any]] = [
+    "明早见哦！",
+    "睡个好觉哦！",
+    "呼呼——",
+    "zzz……",
+    "放下手机睡觉吧！",
+    "做个好梦！",
+    "明天见！",
+    UniMessage().emoji(id="202").emoji(id="202").emoji(id="202"),
+    UniMessage().emoji(id="75").emoji(id="75"),
+    UniMessage().emoji(id="8"),
+]
+
+SLEEP_LATE_MESSAGE = (
+    UniMessage("明天别睡太晚了！早睡早起身体好！").emoji(id="30").emoji(id="30")
+)
+
+
+async def reply_messages(
+    ctx: GroupContext,
+    name: str | None,
+    awarding: int,
+    count: int,
+) -> None:
     rep_name = ""
     if name is not None:
         rep_name = name + "！"
 
     await ctx.reply(
-        UniMessage(f"晚安！{rep_name}")
-        + get_random().choice(
-            [
-                "明早见哦！",
-                "睡个好觉哦！",
-                "呼呼——",
-                "zzz……",
-                "放下手机睡觉吧！",
-                "做个好梦！",
-                "明天见！",
-                UniMessage().emoji(id="202").emoji(id="202").emoji(id="202"),
-                UniMessage().emoji(id="75").emoji(id="75"),
-                UniMessage().emoji(id="8"),
-            ]
-        ),
+        UniMessage(f"晚安！{rep_name}") + get_random().choice(GOODNIGHT_MESSAGES),
         ref=True,
         at=False,
     )
 
     await asyncio.sleep(get_random().random() + 0.5)
     if awarding <= 0:
-        await ctx.reply(
-            UniMessage("明天别睡太晚了！早睡早起身体好！").emoji(id="30").emoji(id="30")
-        )
+        await ctx.reply(SLEEP_LATE_MESSAGE)
     else:
         await ctx.reply(
-            UniMessage(
-                f"奖励早睡的孩子 {awarding} 薯片哦，你已经坚持早睡了 {count} 天了！"
-            )
+            f"奖励早睡的孩子 {awarding} 薯片哦，你已经坚持早睡了 {count} 天了！"
         )
 
-    await asyncio.sleep(get_random().random() + 0.5)
     await ctx.reply("我不看你的消息了哦，明天见！")
